aee8c9a64e46f2de095f67a6bd4b5adbbe7df82b
Siddhant Khurana <94681983+sidkhurana2002@users.noreply.github.com>
2023-10-19 20:46:51 +0530

    mongo_db_atlas

--- config/db.js
+++ config/db.js
@@ -1,19 +1,37 @@
-// config/db.js
-const mongoose = require("mongoose");
+// // config/db.js
+// const mongoose = require("mongoose");
 
+// const connectDB = async () => {
+//   try {
+//     // Connect to MongoDB using the connection string
+//     await mongoose.connect("mongodb://127.0.0.1:27017/Mbackend", {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       //   useCreateIndex: true,
+//       //   useFindAndModify: false,
+//     });
+
+//     console.log("MongoDB Connected");
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+const mongoose = require("mongoose");
+const MONGO_URI =
+  "mongodb+srv://sidakhurana23:sid@cluster0.o94ildg.mongodb.net/?retryWrites=true&w=majority";
 const connectDB = async () => {
   try {
-    // Connect to MongoDB using the connection string
-    await mongoose.connect("mongodb://127.0.0.1:27017/Mbackend", {
+    const conn = await mongoose.connect(MONGO_URI, {
+      // This is to avoid warnings in the console
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      //   useCreateIndex: true,
-      //   useFindAndModify: false,
     });
-
-    console.log("MongoDB Connected");
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error.message);
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
